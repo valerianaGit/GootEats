53fbeb474b4b165b26d55ce205e06663aae13d7a
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-06-13 16:42:46 -0500

    Add custom cell file, format and add data

--- GootEats/Controller/ViewController.swift
+++ GootEats/Controller/ViewController.swift
@@ -12,19 +12,30 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
    
 
     @IBOutlet weak var tableView: UITableView!
-    
+    let data = DataSet()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return data.categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as? CategoryCell {
+            cell.configureCell(category: data.categories[indexPath.row])
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
     }
     
 }
