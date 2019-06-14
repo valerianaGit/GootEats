d249a9a52fdbb479e65a6905a7295b4912404783
Valeria Duran Ruiz <valeria.do@gmail.com>
2019-06-13 22:34:29 -0500

    Render images in collection view

--- GootEats/Controller/RecipesSelectionVC.swift
+++ GootEats/Controller/RecipesSelectionVC.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class RecipesSelectionVC: UIViewController {
+class RecipesSelectionVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  
+    
     
     @IBOutlet weak var collectionView: UICollectionView!
     var selectedCategory: String!
-
+    var recipes: [Recipe]!
+    let data = DataSet()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+collectionView.delegate = self
+        collectionView.dataSource = self
+        recipes = data.getRecipes(forCategoryTitle: selectedCategory)
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return recipes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath) as? RecipeCell {
+            let recipe = recipes[indexPath.item]
+            cell.configureCell(recipe: recipe)
+            return cell
+        }
+        return UICollectionViewCell()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.bounds.width
+        let cellDimension = (width/2) - 15
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
+    
+    
 }
